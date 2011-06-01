ffb6ed8be71e572109b24ee1e0c2dc5d24d45435
Jason Torres <jason.e.torres@gmail.com>
2011-06-01 16:28:45 +0800

    Be a good rails citizen. Modularize route

--- lib/monitaur/routes.rb
+++ lib/monitaur/routes.rb
@@ -1,9 +1,14 @@
 require 'action_dispatch'
 
-module ActionDispatch::Routing
-  class Mapper
-    def monitaur
-      match "/monitaur" =>  "monitaur/monitaur#index", :as => :monitaur
+module Monitaur
+  module Routing
+    module MapperExtensions
+      def monitaur
+        match "/monitaur" =>  "monitaur/monitaur#index", :as => :monitaur
+      end
     end
   end
 end
+
+ActionDispatch::Routing::Mapper.send :include, Monitaur::Routing::MapperExtensions
+
